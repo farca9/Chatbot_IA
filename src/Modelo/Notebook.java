404b779748e739e47b7a44d5bf85b1ab4a0a96a8
Azul <mazulrossini@gmail.com>
2019-06-22 19:29:03 -0300

    Agregado DB

--- src/Modelo/Notebook.java
+++ src/Modelo/Notebook.java
@@ -4,11 +4,95 @@ public class Notebook {
 
     private String codigoModelo;
     private String marca;
-    private String procesador; //TODO VER COMO VAMOS A HACER CON LOS DISTINTOS VALORES DE ESTO, CAPAZ VA UN ENUM
+    private String procesador;
+    private String marcaProcesador;
     private Double dimPantalla;
-    private Double cantidadStorage; //TODO VER, PORQUE EN REALIDAD EL TIPO DE ALMACENAMIENTO DEBERIA SER OTRA CARACTERISTICA DISTINA ADEMAS DE CANTIDAD
-    private Double cantidadRam;
-    private String gpu; //TODO VER SI HCAER ENUM
+    private String cantidadStorage; //Solo numero sin tipo
+    private Integer cantidadRam;
+    private String gpu;
     private Double precio;
 
+    public Notebook(String codigoModelo, String marca, String procesador, String marcaProcesador, Double dimPantalla, String cantidadStorage, Integer cantidadRam, String gpu, Double precio) {
+        this.codigoModelo = codigoModelo;
+        this.marca = marca;
+        this.procesador = procesador;
+        this.marcaProcesador = marcaProcesador;
+        this.dimPantalla = dimPantalla;
+        this.cantidadStorage = cantidadStorage;
+        this.cantidadRam = cantidadRam;
+        this.gpu = gpu;
+        this.precio = precio;
+    }
+
+    public String getCodigoModelo() {
+        return codigoModelo;
+    }
+
+    public void setCodigoModelo(String codigoModelo) {
+        this.codigoModelo = codigoModelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getProcesador() {
+        return procesador;
+    }
+
+    public void setProcesador(String procesador) {
+        this.procesador = procesador;
+    }
+
+    public String getMarcaProcesador() {
+        return marcaProcesador;
+    }
+
+    public void setMarcaProcesador(String marcaProcesador) {
+        this.marcaProcesador = marcaProcesador;
+    }
+
+    public Double getDimPantalla() {
+        return dimPantalla;
+    }
+
+    public void setDimPantalla(Double dimPantalla) {
+        this.dimPantalla = dimPantalla;
+    }
+
+    public String getCantidadStorage() {
+        return cantidadStorage;
+    }
+
+    public void setCantidadStorage(String cantidadStorage) {
+        this.cantidadStorage = cantidadStorage;
+    }
+
+    public Integer getCantidadRam() {
+        return cantidadRam;
+    }
+
+    public void setCantidadRam(Integer cantidadRam) {
+        this.cantidadRam = cantidadRam;
+    }
+
+    public String getGpu() {
+        return gpu;
+    }
+
+    public void setGpu(String gpu) {
+        this.gpu = gpu;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
 }
