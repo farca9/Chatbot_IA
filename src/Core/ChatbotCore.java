b520d2b6aadbc7ab716c928b39e21597e1df9274
Facundo Arca <farca9@gmail.com>
2019-06-23 10:15:40 -0300

    Resolvedor conflictos

--- src/Core/ChatbotCore.java
+++ src/Core/ChatbotCore.java
@@ -3,12 +3,17 @@ package Core;
 import NLP.Lema;
 import NLP.NLPTool;
 import Produccion.MP;
+import Produccion.MT;
+import Produccion.MTEntry;
 import Produccion.Regla;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ChatbotCore {
+
+    private Regla reglaPrevia=null;
 
     //Singleton
     private static ChatbotCore instance = new ChatbotCore();
@@ -36,12 +41,80 @@ public class ChatbotCore {
         System.out.println(reglasActivas);
 
         //Resolucion de conflictos
+        Regla reglaSeleccionada = this.solveConflict(reglasActivas);
 
         //Ejecucion
 
 
         return output;
 
+    }
+
+    private Regla solveConflict(List<Regla> reglasActivas) {
+
+        List<Regla> aux1=new ArrayList<>();
+        List<Regla> aux2=new ArrayList<>();
+
+        //No duplicacion
+        for (Regla r: reglasActivas){
+            if(!r.equals(reglaPrevia)){
+                aux1.add(r);
+            }
+        }
+
+        //Especifidad
+        int maximaEspecificidad=0;
+        for(Regla r: aux1){ //Se obtiene cual es la especificidad maxima
+            if(r.getEspecificidad()>maximaEspecificidad){
+                maximaEspecificidad = r.getEspecificidad();
+            }
+        }
+        for(Regla r:aux1){ //Se queda con las reglas mas especificas
+            if(r.getEspecificidad()==maximaEspecificidad){
+                aux2.add(r);
+            }
+        }
+
+        aux1=aux2;
+        aux2=new ArrayList<>();
+
+        //Novedad
+        ArrayList<MTEntry> mt = (ArrayList)MT.getInstance().getElementosMT();
+        for(MTEntry mtEntry : mt){
+            for(Regla r: aux1){
+                if(r.getMtEntriesPresente().contains(mtEntry)){
+                    aux2.add(r);
+                }
+            }
+            if(!aux2.isEmpty()){
+                break;
+            }
+        }
+        if(!aux2.isEmpty()){
+            aux1=aux2;
+            aux2=new ArrayList<>();
+        }
+
+        //Prioridad
+        int candidatePriority=Integer.MAX_VALUE;
+        for(Regla r: aux1){
+            if(r.getId()<candidatePriority){
+                candidatePriority=r.getId().intValue();
+                aux2=new ArrayList<>();
+                aux2.add(r);
+            } else if (r.getId()==candidatePriority){
+                aux2.add(r);
+            } else {
+                //Se descarta por tener prioridad menos relevante que alguna otra regla
+            }
+        }
+
+        aux1=aux2;
+
+        if(aux1.isEmpty()) return null;
+
+        //Aleatorio
+        return aux1.get(new Random().nextInt(aux1.size()));
     }
 
     public void clearChat(){
--- src/Produccion/Regla.java
+++ src/Produccion/Regla.java
@@ -63,4 +63,44 @@ public class Regla {
         return mensajeOutput;
     }
 
+    public List<MTEntry> getMtEntriesPresente() {
+        return mtEntriesPresente;
+    }
+
+    @Override
+    public String toString() {
+        String retorno="";
+        retorno+="R#"+id+" [";
+        boolean e1=true, e2=true, e3=true, e4=true;
+        for(Lema l: lemasCondicion){
+            retorno+=l.getConcepto()+", ";
+            e1=false;
+        }
+        if(!e1) retorno=retorno.substring(0,retorno.length()-2);
+        retorno+="] <";
+        for(MTEntry mtEntry: mtEntriesPresente){
+            retorno+=mtEntry.toString()+", ";
+            e2=false;
+        }
+        if(!e2)retorno=retorno.substring(0,retorno.length()-2);
+        for(MTEntry mtEntry: mtEntriesAusente){
+            retorno+="!"+mtEntry.toString()+", ";
+            e3=false;
+        }
+        if(!e3)retorno=retorno.substring(0,retorno.length()-2);
+        retorno+="> --> <";
+        for(MTEntry mtEntry: mtEntriesConsecuencia){
+            retorno+=mtEntry.toString()+", ";
+            e4=false;
+        }
+        if(!e4)retorno=retorno.substring(0,retorno.length()-2);
+        retorno+="> '"+mensajeOutput+"'";
+        return retorno;
+
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
 }
